cdb2f7dd295e70f6911e2d8050ba9f6b8fb01c28
madosuki <yurikasamaaikatsu@gmail.com>
2017-02-04 10:32:00 +0900

    Add Files

---
+++ GetTwiImages/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace GetTwiImages.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// アセンブリに関する一般情報は、以下の属性セットによって
+// 制御されます。アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更します。
+[<assembly: AssemblyTitle("GetTwiImages")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("GetTwiImages")>]
+[<assembly: AssemblyCopyright("Copyright ©  2017")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// ComVisible を false に設定すると、COM コンポーネントがこのアセンブリ内のその型を認識
+// できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、
+// その型の ComVisible 属性を true に設定します。
+[<assembly: ComVisible(false)>]
+
+// このプロジェクトが COM に公開される場合、次の GUID がタイプ ライブラリの ID になります
+[<assembly: Guid("5e8ff1e6-472c-4c81-b792-33d012f76c61")>]
+
+// アセンブリのバージョン情報は、以下の 4 つの値で構成されます。:
+// 
+//       メジャー バージョン
+//       マイナー バージョン 
+//       ビルド番号
+//       リビジョン
+// 
+// すべての値を指定するか、下に示すように '*' を使用してビルドおよびリビジョン番号を
+// 既定値にすることができます。:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ GetTwiImages/Program.fs
@@ -0,0 +1,92 @@
+﻿open System
+open System.Net
+open System.Text.RegularExpressions
+open System.IO
+
+let fileList filepath =
+    seq{
+        use sr = new StreamReader(filepath:string)
+        while not sr.EndOfStream do
+            yield sr.ReadLine()
+    }
+
+let addList path =
+    let lists = fileList path
+    lists
+
+let (|Pattern|_|) regex str =
+    let result = Regex(regex).Match(str)
+    if result.Success then
+        Some(result.Groups.[1].Value)
+    else
+        None
+
+
+let checkImage url =
+    match url with
+    | Pattern "(?!.*/)(\w*\.jpg|png)" url -> url
+    | _ -> "None"
+
+let dlImage url dir =
+    printfn "Starting Download Sequence"
+    let savefilename = checkImage url
+    let wc = new WebClient()
+    let saveurl = url + ":orig"
+    let savedir = dir + "\\" + savefilename
+    wc.DownloadFile(saveurl, savedir)
+
+type Help (n) as self =
+    do
+        self.PutHelpText(n)
+    member this.PutHelpText(n) =
+        if n = 1 then
+           printfn "
+
+        Example:
+            GetTwiImages.exe --savedir DirectoryName --list TextfilePath
+
+            --savedir This is Directory to save files. Or not appoint Case is substitute MyPicutres Dir.
+        
+            --list This appoint Text file with download list written."
+
+
+[<EntryPoint>]
+let main argv = 
+    if argv.Length <> 0 then
+        let mutable savedir = ""
+        let mutable listtxt = ""
+        let mutable dir = ""
+        let mutable num = 0
+        for a in argv do
+            if a = "--savedir" then
+                num <- num + 1
+                savedir <- argv.[num]
+                num <- num - 1
+            elif a = "--list" then
+                num <- num + 1
+                listtxt <- argv.[num]
+                num <- num - 1
+            num <- num + 1
+        
+        if savedir = "" then
+           savedir <- System.Environment.GetFolderPath(Environment.SpecialFolder.MyPictures)
+
+        if listtxt <> "" then
+            let urllist = addList listtxt
+
+            for i in urllist do
+                dlImage i savedir
+
+        elif listtxt = "" then
+            for b in argv do
+                if checkImage(b) = "None" then
+                    let h = new Help(1)
+                    System.Environment.Exit(0)
+                else
+                    dlImage b savedir
+                    
+    else
+        let h = new Help(1)
+        System.Environment.Exit(0)
+
+    0
